ef862c199da5b3e5ac7891507f52e64fa80224fa
AliyahAunJo <aliyahaunjo@gmail.com>
2023-08-27 18:25:01 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+      var path = new URL(window.location);
+      var liffId = path.searchParams.get('linkurl');
+        liff.init({
+          liffId: "xxxxxxxx" //วางLiffId ตรงนี้
+        })
+        .then(() => {
+          if (liff.isInClient()) {
+            if (liff.getOS() == "ios") {
+              window.location.replace(""+liffId);              
+            } else if (liff.getOS() == "android") {
+              window.location.replace(""+liffId);
+            }
+
+            setTimeout(function(){ liff.closeWindow(); }, 3000); //เปลี่ยนเวลาที่ต้องการ Redirect
+          }
+        })
+        .catch((err) => {
+        });
+      });
+
+  // Check if LIFF is initialized
+if (liff.isInClient()) {
+    // Add a click event listener to the close button
+    document.getElementById("closeButton").addEventListener("click", function() {
+        // Close the LIFF app when the button is clicked
+        liff.closeWindow();
+    });
+} else {
+    // Handle the case when the app is not running in the LINE app
+    console.log("This app is not running in the LINE app.");
+}
