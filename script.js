66a07d7d872b587320fecb78178941272afa9bc2
AliyahAunJo <aliyahaunjo@gmail.com>
2023-09-19 14:00:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,9 @@ $(document).ready(function() {
         .then(() => {
           if (liff.isInClient()) {
             if (liff.getOS() == "ios") {
-              // window.location.replace(""+liffId);              
-              window.location.replace(liffId);              
+              window.location.replace(""+liffId);                      
             } else if (liff.getOS() == "android") {
-              // window.location.replace(""+liffId);
-              window.location.replace(liffId);
+              window.location.replace(""+liffId);
             }
 
             setTimeout(function(){ liff.closeWindow(); }, 3000); //เปลี่ยนเวลาที่ต้องการ Redirect
